902ca939e750a433f08921b55cfe7d83889f7e12
Claus Due <claus@wildside.dk>
2012-11-15 12:50:53 +0100

    [TASK] Enable posting via AJAX in Comment Widget

--- Resources/Public/Javascripts/Plugins/Comment.js
+++ Resources/Public/Javascripts/Plugins/Comment.js
@@ -10,24 +10,35 @@
 			var loadCommentForm = function() {
 				element.load(urlForm, function() {
 					element.removeClass('placeholder').addClass('full').unbind('click');
-					element.find('.btn-cancel').attr('href', 'javascript:;');
-					element.click(function() {
-						element.load(urlClose, function(e) {
-							element.addClass('placeholder').removeClass('full');
-						});
-						element.click(loadCommentForm);
-					});
+					applyCommentFormListeners();
 				});
 			};
-			element.find('.btn-cancel').live('click', function() {
-				element.load(urlClose, function(e) {
+			var closeCommentForm = function() {
+				element.load(urlClose, function() {
 					element.addClass('placeholder').removeClass('full');
-					element.click(loadCommentForm);
-					e.cancel();
+					element.bind('click', loadCommentForm);
 				});
-			});
+			};
+			var applyCommentFormListeners = function() {
+				element.find('.btn-cancel').attr('href', 'javascript:;').bind('click', closeCommentForm);
+				element.unbind('click', loadCommentForm);
+				element.find('button[type="submit"]').bind('click', submitCommentForm);
+			};
+			var submitCommentForm = function() {
+				var postData = {
+					subject: element.find('#subject').val(),
+					comment: element.find('#words').val()
+				};
+				jQuery.ajax({
+					url: urlPost,
+					type: 'post',
+					data: postData,
+					complete: loadCommentForm
+				});
+			};
+			element.find('.btn-cancel').live('click', closeCommentForm);
 			if (element.hasClass('placeholder')) {
-				element.click(loadCommentForm);
+				element.bind('click', loadCommentForm);
 			} else {
 				loadCommentForm();
 			};
