e06699c2abe5c073636a5552cf3f4a6e12c41a3b
Claus Due <claus@wildside.dk>
2012-12-28 03:44:55 +0100

    [FEATURE] Auto-expanding textarea height

--- Resources/Public/Javascripts/Plugins/Init.js
+++ Resources/Public/Javascripts/Plugins/Init.js
@@ -1,3 +1,45 @@
 function initializeJQueryWidget(elementId, widgetType, options, instanceName) {
 	var localTimer = setInterval(function() { if (typeof jQuery != 'undefined') { window[instanceName] = jQuery('#' + elementId)[widgetType](options); clearInterval(localTimer); }; }, 250);
 };
+jQuery(document).ready(function($) {
+	var timer;
+	var resetTimer = function() {
+		clearInterval(timer);
+		timer = setTimeout(resizeAllTextAreas, 100);
+	};
+	$('.tx-dialog-discussion textarea')
+		.keyup(resetTimer)
+		.focus(resetTimer)
+		.blur(resetTimer);
+	resetTimer();
+});
+
+
+var resizeAllTextAreas = function() {
+	$('textarea.expanding').each(function() {
+		var area = $(this).removeAttr('rows').removeAttr('cols').removeClass('input-xlarge');
+		var shouldBeHeight = area.get(0).scrollHeight;
+		var isHeight = area.innerHeight();
+		var space = 0;
+		if (shouldBeHeight > isHeight) {
+			area.css({
+				"height": shouldBeHeight + space,
+				"maxHeight": shouldBeHeight + space,
+				"minHeight": shouldBeHeight + space
+			});
+		} else {
+			area.css({
+				"height": space,
+				"maxHeight": space,
+				"minHeight": space
+			});
+			isHeight = area.innerHeight();
+			shouldBeHeight = area.get(0).scrollHeight;
+			area.css({
+				"height": shouldBeHeight + space,
+				"maxHeight": shouldBeHeight + space,
+				"minHeight": shouldBeHeight + space
+			});
+		};
+	});
+};
